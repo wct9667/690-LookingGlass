b9bb9c2e5587784d3ce773802d45ef6ff544c381
William Tallarico <wct9667@rit.edu>
2024-09-25 16:30:09 -0400

    changes

--- Looking_Glass/Assets/Swooping.cs
+++ Looking_Glass/Assets/Swooping.cs
@@ -65,7 +65,7 @@ public class Swooping : MonoBehaviour
             if (shotTime > shotFrequency)
             {
                 shotTime = 0;
-                Instantiate(projectile, new Vector3(ship.position.x, ship.position.y, ship.position.z), new Quaternion(ship.rotation.x + Random.Range(-5,5), ship.rotation.y, ship.rotation.z, ship.rotation.w));
+                Instantiate(projectile, new Vector3(ship.position.x, ship.position.y, ship.position.z), new Quaternion(ship.rotation.x + Random.Range(-5,5), ship.rotation.y + Random.Range(-1,1), ship.rotation.z , ship.rotation.w));
             }
         }
         else
